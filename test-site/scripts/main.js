ca52bcdc6c5c4e0dd4e97ca59b020de7eff76b9a
jonask <kjonas697@gmail.com>
2021-07-13 15:29:27 -0600

    updating web files to reflect the name change button (not working)

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,2 +1,29 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World!';
+alert('hi there!');
+let myImage = document.querySelector ('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute ('src');
+    if(mySrc === 'images/Firefox.png') {
+        myImage.setAttribute('src','images/Firefox2.png');
+    } else { 
+        myImage.setAttribute('src','images/Firefox.png');
+    }
+}
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is Cool, ' + myName;
+}
+let myButton = document.querySelector ('button');
+let myHeading = document.querySelector ('h1');
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else { 
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is Cool, ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+}
